550cb202f4bd340ed5adf27bead0dddbb372cd4c
ruanmuller20 <ruanmuller20@gmail.com>
2025-10-28 15:33:48 -0300

    Parâmetros de Rota

--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,53 @@ app.get('/', (req, res) => {
   res.send('Hello World - tsc!');  
 });
 
+type User = {
+  id: number;
+  nome: string;
+  email: string
+};
 
-let usuarios = [{
-  nome: 'Derci Santos',
-  idade: 33
-},{
-  nome: 'João da Silva',
-  idade: 44
-}];
+
+
+let id = 0;
+let usuarios : User[] = [];
 
 
 app.get ('/users', (req: Request, res: Response) => {
   res.send(usuarios); 
 });
 
+app.get('/users/:id', (req: Request, res: Response) => {
+  let userId = Number(req.params.id);
+  let user = usuarios.find(user => user.id === userId);
+  res.send(user);
+});
+
+app.put('/users/:id', (req: Request, res: Response) => {
+  let userId = Number(req.params.id);
+  let user = req.body;
+  let indexOf = usuarios.findIndex((_user: User) => _user.id === userId);
+  usuarios[indexOf].nome = user.nome;
+  usuarios[indexOf].email = user.email;
+  res.send({
+    message: 'Usuário alterado com sucesso!',
+  })
+});
+
+app.delete('/users/:id', (req: Request, res: Response) => {
+  let userId = Number(req.params.id);
+  let indexOf = usuarios.findIndex((user: User) => user.id === userId);
+  usuarios.splice(indexOf, 1);
+  res.send({
+    message: 'Usuário deletado com sucesso!',
+  });
+
+});
+
+
 app.post('/users', (req: Request, res: Response) => {  
  let user = req.body
+ user.id = ++id;
  usuarios.push(user);
  res.send({
   message: 'Usuário criado com sucesso!',
